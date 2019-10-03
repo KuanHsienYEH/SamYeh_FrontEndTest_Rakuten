bc425fabb3e8262f5bc210d5e4abe277f6091b71
KuanHsienYeh <knight123g@gmail.com>
2019-10-03 14:52:56 +0800

    rebuild

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/SamYeh_FrontEndTest_Rakuten/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
